787c68f15cec6d52af1a6d9f1acd586b6bb2c1f9
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-09-08 12:36:57 -0300

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,24 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => "gmail.com", 
+   :user_name            => "sebastian.degrandi@gmail.com",
+   :password             => "sD1976k.",
+   :authentication       => "plain",
+   :enable_starttls_auto => true,
+   :openssl_verify_mode  => 'none'
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'http://sapofx.herokuapp.com', port: 80 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb.bak
+++ config/routes.rb.bak
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'principal/index'
-  root 'principal#index'
+  root to: "admin/dashboard#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'principal/index'
+  #root 'principal#index'
 end
